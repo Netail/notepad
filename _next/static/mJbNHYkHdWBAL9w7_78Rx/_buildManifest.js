fbe9920cd4a46b58a7ad63b17f3ca07498947a20
Netail <maikel.van.dort@gmail.com>
2022-11-30 22:26:06 +0000

    Deploying to gh-pages from @ Netail/notepad@08a504d3b27e78f4158b6283cbdc1350935ad0eb 🚀

--- _next/static/mJbNHYkHdWBAL9w7_78Rx/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/edfb97e7c7108fde.css","static/chunks/pages/index-ff78dc7a58d24488.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
