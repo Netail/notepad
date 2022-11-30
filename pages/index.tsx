08a504d3b27e78f4158b6283cbdc1350935ad0eb
Netail <maikel.van.dort@gmail.com>
2022-11-30 23:25:13 +0100

    fix: favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,19 @@ import Head from 'next/head';
 import { useState } from 'react';
 import Navigation from '../components/navigation/navigation';
 import Footer from '../components/footer/footer';
+import { useRouter } from 'next/router';
 
 import css from '../styles/index.module.scss';
 
 const Home: NextPage = () => {
+    const router = useRouter();
     const [touched, setTouched] = useState(false);
 
     return (
         <>
             <Head>
                 <title>{touched ? '*' : ''}Untitled - Notepad</title>
-                <link rel='shortcut icon' href='/favicon.ico' />
+                <link rel='shortcut icon' href={`${router.basePath}/favicon.ico`} />
             </Head>
             <main className={css.layout}>
                 <Navigation />
